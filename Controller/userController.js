5837fcbb46c75cc1b303f7b79b79a6bcbf5b65c0
Bhakti Awate <bhaktiawate1@gmail.com>
2024-08-17 15:40:18 -0700

    changes

--- Controller/userController.js
+++ Controller/userController.js
@@ -1108,22 +1108,22 @@ exports.getProject = async (req, res) => {
     }
 }
 
-exports.getUserProfileDashboard = async (req, res) => {
-    try {
-        const userId = req.user.id; // Assuming user ID is stored in req.user after authentication
-        const user = await User.findByPk(userId, {
-            attributes: ['profile', 'name', 'deptId', 'roleId', 'id']
-        });
+// exports.getUserProfileDashboard = async (req, res) => {
+//     try {
+//         const userId = req.user.id; // Assuming user ID is stored in req.user after authentication
+//         const user = await User.findByPk(userId, {
+//             attributes: ['profile', 'name', 'deptId', 'roleId', 'id']
+//         });
 
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
+//         if (!user) {
+//             return res.status(404).json({ message: 'User not found' });
+//         }
 
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({ message: 'Internal server error', error: error.message });
-    }
-};
+//         res.status(200).json(user);
+//     } catch (error) {
+//         res.status(500).json({ message: 'Internal server error', error: error.message });
+//     }
+// };
 
 exports.taskTimer = async (req, res) => {
     try {
